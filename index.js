a8da447d45f174e29a5c990d622ef38bdbf19b81
Excel You <tamilprimeacetech90@gmail.com>
2026-04-24 17:03:30 +0530

    fix package.json

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-
-mongoose.connect("mongodb+srv://tamilprimeacetech90:adhiyeTHUNAI@90+++@cluster0.xezkqea.mongodb.net/excelyou?retryWrites=true&w=majority")
+const Message = require("./models/Message");
+mongoose.connect("mongodb+srv://TESTING:Testing123@cluster0.xezkqea.mongodb.net/excelyou?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => console.log("MongoDB Connected ✔"))
 .catch(err => console.log(err));
-
-
 app.use(express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
 
@@ -24,10 +22,34 @@ app.get("/contact", (req, res) => {
     res.sendFile(path.join(__dirname, "contact.html"));
 });
 
-app.post("/contact", (req, res) => {
-    res.send("<h1>Message Received 👍</h1><a href='/'>Go Back</a>");
+app.post("/contact", async (req, res) => {
+    const { name, email, message } = req.body;
+
+    await Message.create({
+        name,
+        email,
+        message
+    });
+
+    res.send("<h1>Message Saved in Database 👍</h1><a href='/'>Go Back</a>");
 });
 
+app.get("/admin", async (req, res) => {
+    const messages = await Message.find();
+
+    let html = "<h1>Admin Panel 📊</h1><a href='/'>Home</a><hr>";
+
+    messages.forEach(m => {
+        html += `
+        <div style="margin:10px;padding:10px;border:1px solid #ccc;">
+            <b>Name:</b> ${m.name} <br>
+            <b>Email:</b> ${m.email} <br>
+            <b>Message:</b> ${m.message}
+        </div>`;
+    });
+
+    res.send(html);
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
