e8ae0471b0ceacd25849efb3dee664e061a32138
Excel You <tamilprimeacetech90@gmail.com>
2026-04-24 13:18:51 +0530

    fix crash clean server

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require("path");
 app.use(express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
 
-// Pages
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -18,34 +17,8 @@ app.get("/contact", (req, res) => {
     res.sendFile(path.join(__dirname, "contact.html"));
 });
 
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Server running");
-});   
-
-
-
-const PORT = process.env.PORT || 3000;
-
-// Admin route (MUST be before listen)
-app.get("/admin", (req, res) => {
-    db.all("SELECT * FROM messages", (err, rows) => {
-        let html = "<h1>Admin Panel 📊</h1><a href='/'>Home</a><hr>";
-
-        rows.forEach(row => {
-            html += `
-                <div style="margin:10px;padding:10px;border:1px solid #ccc;">
-                    <b>Name:</b> ${row.name} <br>
-                    <b>Email:</b> ${row.email} <br>
-                    <b>Message:</b> ${row.message}
-                </div>
-            `;
-        });
-
-        res.send(html);
-    });
+app.post("/contact", (req, res) => {
+    res.send("<h1>Message Received 👍</h1><a href='/'>Go Back</a>");
 });
 
 const PORT = process.env.PORT || 3000;
